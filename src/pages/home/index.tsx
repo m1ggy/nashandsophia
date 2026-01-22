2e132f41548d18d29d5a02640f8409496e2e90be
M1gs <miguel.buising1@gmail.com>
2026-01-22 18:04:47 +0800

    feat: add fonts, add icos, cleanup boilerplate

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,10 @@
-import { Helmet } from 'react-helmet'
 import { useTranslation } from 'react-i18next'
-import { Hero } from 'src/components/hero'
 
 export default function Home() {
   const { t } = useTranslation('translation')
   return (
     <>
-      <Helmet>
-        <title>{t('title')}</title>
-      </Helmet>
-      <Hero />
+      <h1> Nash And Sophia</h1>
     </>
   )
 }
